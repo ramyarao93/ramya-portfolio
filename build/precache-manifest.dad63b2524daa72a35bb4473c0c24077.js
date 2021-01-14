02d2d97ec9d402f5986c1284a8ddbebfdb613a13
ramya_rao <ramya.donekal@itpeoplecorp.com>
2021-01-14 12:01:22 -0500

    profile setup

---
+++ build/precache-manifest.dad63b2524daa72a35bb4473c0c24077.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0e18dd0643448397722ed7be664869",
+    "url": "/ramya-portfolio/index.html"
+  },
+  {
+    "revision": "8177bec3c1b595d9322e",
+    "url": "/ramya-portfolio/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "d5e21a0972c7dc852afb",
+    "url": "/ramya-portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8177bec3c1b595d9322e",
+    "url": "/ramya-portfolio/static/js/2.e1d1dd90.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ramya-portfolio/static/js/2.e1d1dd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e21a0972c7dc852afb",
+    "url": "/ramya-portfolio/static/js/main.a520bf28.chunk.js"
+  },
+  {
+    "revision": "81d4b5a0b5dd3b09be3d",
+    "url": "/ramya-portfolio/static/js/runtime-main.51531a00.js"
+  },
+  {
+    "revision": "c6066ba1c34aa0a019fd3b01f41981a7",
+    "url": "/ramya-portfolio/static/media/Portfolio-About.c6066ba1.jpeg"
+  }
+]);
